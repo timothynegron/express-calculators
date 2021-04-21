2fcad5175b01b47672f5fdc32ab31457776e8662
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-20 21:58:19 -0400

    Add two numbers complete

--- calculator.js
+++ calculator.js
@@ -1,13 +1,24 @@
 const express = require ("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", function(req, res){
-    res.send("Thanks for posting that!");
+
+    //console.log(req.body.num1);
+
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    const result = Number(num1) + Number(num2);
+
+    res.send("The result of the calculation is " + result);
 })
 
 app.listen(port, function(){
