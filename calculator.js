74cfbc86cc426506c67e110fc7d748bca40c3782
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-20 22:21:30 -0400

    Adds BMI calculator

--- calculator.js
+++ calculator.js
@@ -12,13 +12,24 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    //console.log(req.body.num1);
-
     const num1 = req.body.num1;
     const num2 = req.body.num2;
     const result = Number(num1) + Number(num2);
 
     res.send("The result of the calculation is " + result);
+})
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmi-calculator.html")
+})
+
+app.post("/bmicalculator", function(req, res){
+    
+    const height = parseFloat(req.body.height);
+    const weight = parseFloat(req.body.weight);
+    const bmi = weight / (height * height);
+
+    res.send("BMI Calculation: " + bmi.toFixed(2));
 })
 
 app.listen(port, function(){
