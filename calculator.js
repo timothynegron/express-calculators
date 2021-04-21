a246f455162d71f5adada9b409acba903885f78d
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-20 21:27:24 -0400

    Adds code to do something for post

--- calculator.js
+++ calculator.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", function(req, res){
-    res.send("Hello, World!");
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    res.send("Thanks for posting that!");
 })
 
 app.listen(port, function(){
